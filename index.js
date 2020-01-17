8e755854e3746caabef791ee5e9b857c9dd885e2
Warren Yao <warrenyca905@gmail.com>
2020-01-17 18:57:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign(driver, { [key]: value });
 }
+
+function deleteFromDriverByKey(driver, key) {
+  const temp = Object.assign({}, driver);
+  delete temp[key];
+  return temp;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
